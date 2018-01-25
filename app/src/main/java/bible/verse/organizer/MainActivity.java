485a619db0a59173a9badcd3d557b466bd23c5c5
esfox <osairis100@gmail.com>
2018-01-25 15:35:14 +0800

    Added theme colors by Gigabyte, Created draft verse card (for home page list)

--- app/src/main/java/bible/verse/organizer/MainActivity.java
+++ app/src/main/java/bible/verse/organizer/MainActivity.java
@@ -13,7 +13,6 @@ import bible.verse.organizer.organizer.R;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,16 +22,7 @@ public class MainActivity extends AppCompatActivity
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
+        //TODO: Launch Home fragment
     }
 
     @Override

1ced1186445eb7c168eb6021b2b63062d4078041
esfox <osairis100@gmail.com>
2018-02-19 13:49:08 +0800

    Completed Verse Citation Input

--- app/src/main/java/bible/verse/organizer/MainActivity.java
+++ app/src/main/java/bible/verse/organizer/MainActivity.java
@@ -1,17 +1,9 @@
 package bible.verse.organizer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import bible.verse.organizer.fragments.Home;
 import bible.verse.organizer.organizer.R;

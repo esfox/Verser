1c8f16c6b556324ad9a49ec8a7bc2b74ad915c26
esfox <osairis100@gmail.com>
2018-03-17 17:07:48 +0800

    Updated Tags Item Layout

--- app/src/main/java/bible/verse/organizer/fragments/Home.java
+++ app/src/main/java/bible/verse/organizer/fragments/Home.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
